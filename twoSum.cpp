7cc8665d0949e865833063e30c950b879bf9475d
BrianWu <brianwu291@gmail.com>
2021-09-03 14:56:34 +0800

    towSum

---
+++ twoSum.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+
+class Solution {
+
+  public:
+
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+      std::unordered_map<int, int> numberIndexMap;
+
+      int numsSize = nums.size();
+
+      for (int i = 0; i < numsSize; i += 1) {
+        int num = nums[i];
+
+        int rest = target - num;
+
+        int numberNotExist = numberIndexMap.count(num) == 0;
+
+        if (numberNotExist) {
+          numberIndexMap[rest] = i;
+        } else {
+          std::vector<int> result { i, numberIndexMap[num] };
+          return result;
+        };
+      }
+
+      std::vector<int> result;
+
+      return result;
+    }
+};
