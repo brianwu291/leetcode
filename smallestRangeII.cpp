fe64826c234870a7404311942aab400354b35687
BrianWu <brianwu291@gmail.com>
2021-09-03 15:17:22 +0800

    Smallest Range II.

---
+++ smallestRangeII.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+// https://leetcode.com/problems/smallest-range-ii/
+
+class Solution {
+  public:
+    int smallestRangeII(std::vector<int>& nums, int k) {
+      std::sort(nums.begin(), nums.end());
+
+      int length = nums.size();
+
+      int result = nums[length - 1] - nums[0];
+
+      for (int i = 0; i < length - 1; i += 1) {
+        int max = std::max(nums[i] + k, nums[length - 1] - k);
+        int min = std::min(nums[0] + k, nums[i + 1] - k);
+
+        result = std::min(result, max - min);
+      }
+      
+      return result;
+    }
+};
