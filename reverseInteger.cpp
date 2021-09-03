709bb8f2db5549a3e787ca5aa98736140372fc50
BrianWu <brianwu291@gmail.com>
2021-09-03 15:01:54 +0800

    reverse integer.

---
+++ reverseInteger.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+class Solution {
+  public:
+    int reverse(int x) {
+      int result = 0, rest;
+
+      while (x != 0) {
+        if (result >= 214748365 || result <= -214748365) {
+          return 0;
+        }
+
+        rest = x % 10;
+
+        result = result * 10 + rest;
+
+        x /= 10;
+      }
+
+      return result;
+    }
+};
