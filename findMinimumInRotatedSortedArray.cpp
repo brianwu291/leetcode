7dacd4be888729b0b269d78ea47a188f35350586
BrianWu <brianwu291@gmail.com>
2021-09-05 15:15:55 +0800

    use binary search.

--- findMinimumInRotatedSortedArray.cpp
+++ findMinimumInRotatedSortedArray.cpp
@@ -3,6 +3,11 @@
 
 // https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
 
+// 4,5,6,7,0,1,2
+// 6,7,0,1,2,4,5
+// 3,4,5,1,2
+// 0       4   6
+//       i j
 class Solution {
   public:
     int findMin(std::vector<int>& nums) {
@@ -13,21 +18,16 @@ class Solution {
       if (length == 2) {
         return nums[0] < nums[1] ? nums[0] : nums[1];
       }
-      int result;
-      bool isFound = false;
       int i = 0, j = length - 1;
-      while (i < j && !isFound) {
-        if (nums[i] > nums[i + 1]) {
-          result = nums[i + 1];
-          isFound = true;
+      while (i < j) {
+        int midIndex = (j + i) / 2;
+        int midNum = nums[midIndex];
+        if (midNum > nums[j]) {
+          i = midIndex + 1;
+        } else {
+          j = midIndex;
         }
-        if (nums[j - 1] > nums[j]) {
-          result = nums[j];
-          isFound = true;
-        }
-        i += 1;
-        j -= 1;
       }
-      return result;
+      return nums[i];
     }
 };
