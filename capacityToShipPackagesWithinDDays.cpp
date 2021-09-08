3976d91836d1655d0177d8d1f660b06ea72a1355
BrianWu <brianwu291@gmail.com>
2021-09-08 19:01:33 +0800

    test cout and cin.

---
+++ capacityToShipPackagesWithinDDays.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+  public:
+    int shipWithinDays(std::vector<int>& weights, int days) {
+
+    }
+};
+
+int main() {
+  std::cout << "enter two number: " << std::endl;
+
+  int v = 0, k = 0;
+
+  std::cin >> v >> k;
+
+  std::cout
+      << "v is " << v
+      << " and "
+      << "k is " << k
+      << " and total is "
+      << v + k
+      << std::endl; 
+
+  return 0;
+}
