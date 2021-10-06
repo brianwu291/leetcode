fabf6b6e3b07a2e23ccd865b82b4acece6e0306d
BrianWu <brianwu291@gmail.com>
2021-10-06 23:05:50 +0800

    format pretty.

--- minimumDeletionsToMakeStringBalanced.cpp
+++ minimumDeletionsToMakeStringBalanced.cpp
@@ -12,40 +12,45 @@
 // b b a a a a a b b
 // 2
 
-class Solution {
-  public:
-    std::string A = "a";
-    std::string B = "b";
+class Solution
+{
+public:
+  std::string A = "a";
+  std::string B = "b";
 
-    int minimumDeletions(std::string s) {
-      int length = s.size();
-      // count "b"
-      std::vector<int> pre(length);
-      std::string firstStr;
-      firstStr.push_back(s[0]);
-      pre[0] = firstStr == B ? 1 : 0;
-      for (int i = 1; i < length; i += 1) {
-        std::string curStr;
-        curStr.push_back(s[i]);
-        int count = curStr == B ? 1 : 0;
-        pre[i] = pre[i - 1] + count;
-      }
-      // count "a"
-      std::vector<int> suf(length);
-      std::string lastStr;
-      lastStr.push_back(s[length - 1]);
-      suf[length - 1] = lastStr == A ? 1 : 0;
-      for (int i = length - 2; i >= 0; i -= 1) {
-        std::string curStr;
-        curStr.push_back(s[i]);
-        int count = curStr == A ? 1 : 0;
-        suf[i] = suf[i + 1] + count;
-      }
-      
-      int result = std::min(pre[length - 1], suf[0]);
-      for (int i = 0; i < length - 1; i += 1) {
-         result = std::min(result, pre[i] + suf[i + 1]);
-      }
-      return result;
+  int minimumDeletions(std::string s)
+  {
+    int length = s.size();
+    // count "b"
+    std::vector<int> pre(length);
+    std::string firstStr;
+    firstStr.push_back(s[0]);
+    pre[0] = firstStr == B ? 1 : 0;
+    for (int i = 1; i < length; i += 1)
+    {
+      std::string curStr;
+      curStr.push_back(s[i]);
+      int count = curStr == B ? 1 : 0;
+      pre[i] = pre[i - 1] + count;
     }
+    // count "a"
+    std::vector<int> suf(length);
+    std::string lastStr;
+    lastStr.push_back(s[length - 1]);
+    suf[length - 1] = lastStr == A ? 1 : 0;
+    for (int i = length - 2; i >= 0; i -= 1)
+    {
+      std::string curStr;
+      curStr.push_back(s[i]);
+      int count = curStr == A ? 1 : 0;
+      suf[i] = suf[i + 1] + count;
+    }
+
+    int result = std::min(pre[length - 1], suf[0]);
+    for (int i = 0; i < length - 1; i += 1)
+    {
+      result = std::min(result, pre[i] + suf[i + 1]);
+    }
+    return result;
+  }
 };
