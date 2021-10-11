862e51563c57630ceec9faf46c8f61fe47fe2695
BrianWu <brianwu291@gmail.com>
2021-10-11 22:58:02 +0800

    palindromicSubstrings not finish.

---
+++ palindromicSubstrings.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+// https://leetcode.com/problems/palindromic-substrings/
+
+// acafefbwq
+// 
+
+class Solution {
+  public:
+    int countSubstrings(std::string s) {
+      int length = s.size();
+
+      int result = 0;
+      for (int i = 0; i < length; i += 1)
+      {
+        /* code */
+      }
+      
+    }
+};
