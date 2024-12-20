b87d44c454bb9ee37f513bbe2219dcfaeb37d0fb
BrianWu <brianwu291@gmail.com>
2024-12-20 23:41:30 +0800

    feat: add first golang leetcode practice

--- leetcodeInGolang/towSum.go
+++ leetcodeInGolang/towSum.go
@@ -1,28 +1,32 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	nums := []int{ 2, 7, 11, 15 }
-	solution(nums, 22)
+	nums := []int{2, 0, 1, 4, 3}
+	res := solution(nums, 6)
+	fmt.Printf("res: %+v \n", res)
 }
 
 func solution(nums []int, target int) []int {
-	result := make([]int, 0)
+	type Result []int
+	// { [target - cur]: index, ... }
+	// each time read with cur, if exist means match
+	result := Result{}
+	tempMap := make(map[int]int)
 
-	type NumInfo struct {
-			index int
-			val  int
+	for i := 0; i < len(nums); i += 1 {
+		curNum := nums[i]
+		matchIndex, exists := tempMap[curNum]
+		if exists {
+			result = append(result, i, matchIndex)
+			break
+		}
+		gap := target - curNum
+		tempMap[gap] = i
 	}
 
-	sumMap := make(map[int]NumInfo)
-	sumMap[target - nums[0]] = NumInfo{ index: 0, val: nums[0] }
-	hasFounded := false
-	for i := 1; i < len(nums); i += 1 {
-			if matchedNumInfo, exist := sumMap[nums[i]]; exist && !hasFounded {
-					result = append(result, matchedNumInfo.index, i)
-					hasFounded = true;
-			} else {
-					sumMap[target - nums[i]] = NumInfo{ index: i, val: nums[i] }
-			}
-	}
 	return result
 }
